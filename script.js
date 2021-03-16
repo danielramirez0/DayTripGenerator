f11cd6980888f8024483a329bf5f20e097bf3f92
Daniel Ramirez <notused@notused.net>
2021-03-15 19:54:05 -0700

    Created arrays for prime trip generator

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const destinations = [
+  "Hoth",
+  "Tatooine",
+  "Coruscant",
+  "Naboo",
+  "Kashyyyk",
+  "Dagobah",
+];
+const restaurants = [
+  "Docking Bay 7 Food and Cargo (quick service dining)",
+  "Oga’s Cantina (bar/lounge)",
+  "Milk Stand (snack stand)",
+  "Ronto Roasters (quick service)",
+  "Kat Sakas Kettle (snack stand)",
+];
+
+const transportation = [
+  "All Terrain Armored Transport (AT-AT)",
+  "All Terrain Scout Transport (AT-ST)",
+  "Snowspeeder (T-47 Airspeeder)",
+  "ETA-2 Jedi Starfighter",
+  "Y-Wing Fighter",
+  "X-Wing Fighter",
+  "TIE Interceptor",
+  "Slave I",
+  "Millennium Falcon",
+];
+
+const entertainment = [
+  "Podracing",
+  "Droid Boxing",
+  "Jedi Temple Experience",
+  "Nightclub Dancing",
+  "Live Music",
+];
